d5c901531a3b1e594d927eecc44e76dc0faf8f4f
sumeetprajapati1996 <sumeetprajapati15@gmail.com>
2021-05-15 15:53:10 +0530

    15-05-2021

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TransferTypeSeeder::class,
-            GodownSeeder::class,
-            ProductSeeder::class,
+            // GodownSeeder::class,
+            // ProductSeeder::class,
         ]);
     }
 }
